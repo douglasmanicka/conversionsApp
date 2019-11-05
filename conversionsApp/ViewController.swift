6a6fc7a5b5223481820896f5de2b43c418a056ec
Douglas Manicka <manicka@MacBook-Pro-de-Douglas.local>
2019-11-05 16:45:52 -0300

    project base

---
+++ conversionsApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  conversionsApp
+//
+//  Created by Douglas Manicka on 05/11/19.
+//  Copyright © 2019 Douglas Manicka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+   
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
